7c41011499ec48bac09f4f64ec8dfdfa6af3e0d3
crackheadakira <lasn.mine@gmail.com>
2022-08-23 17:14:51 +0200

    Add Vite files

--- js/novel.js
+++ js/novel.js
@@ -1,37 +1,74 @@
-import $ from 'jquery';
+import $ from "jquery";
+import showdown from "showdown";
+const converter = new showdown.Converter();
 
-let chapterCount = createChapterCount(6);
-let chapterTitles = ["CHAPTER 1 - THE VISIT", "CHAPTER 2 - A DISCUSSION WITH GOD",
-    "CHAPTER 3 - MISFORTUNE", "CHAPTER 4 - THE TRIAL",
-    "CHAPTER 5 - ESCAPE", "CHAPTER 6 - THE BUILDING"];
+// CHAPTER CODE
 
-for (const chapter of chapterCount) {
-    $.get(`../assets/chapters/${chapter}.md`, function (data) {
-        data = data.replace(/(?:\r\n|\r|\n)/g, '<br>');
+let chapterLength = await (
+  await fetch(
+    "https://raw.githubusercontent.com/crackheadakira/novel/main/chapters/chapterCount.md"
+  )
+).text();
 
-        let chapterDiv = document.createElement('div');
-        let chapterTitle = document.createElement('div');
-        let chapterText = document.createElement('div');
+let chapterCount = createChapterCount(chapterLength);
 
-        chapterDiv.classList.add('chapter');
-        chapterTitle.classList.add('chapterTitle');
-
-        chapterDiv.id = chapter;
-
-        chapterTitle.textContent = chapterTitles[chapterCount.indexOf(chapter)];
-        chapterText.innerHTML = data;
-
-        chapterDiv.appendChild(chapterTitle);
-        chapterDiv.appendChild(chapterText)
-        document.querySelector('#chapters').insertBefore(chapterDiv, document.querySelector('#footer'));
+for (let i = 0; i < chapterCount.length; i++) {
+  await fetch(
+    "https://raw.githubusercontent.com/crackheadakira/novel/main/chapters/" +
+      chapterCount[i] +
+      ".md"
+  )
+    .then((response) => response.text())
+    .then((data) => {
+      const title = data.slice(0, data.indexOf("\n"));
+      data = data
+        .replace(title, "")
+        .replace("\n", "")
+        .replace(/(?:\r\n|\r|\n)/g, "<br>");
+      const chapterDiv = document.createElement("div");
+      const chapterTitle = document.createElement("div");
+      const chapterText = document.createElement("div");
+      const chapterItem = document.createElement("a");
+      chapterDiv.classList.add("chapter");
+      chapterTitle.classList.add("chapterTitle");
+      chapterDiv.id = chapterCount[i];
+      chapterItem.href = `#${chapterCount[i]}`;
+      chapterItem.textContent = `CHAPTER ${i + 1}`;
+      chapterTitle.textContent = title;
+      chapterText.innerHTML = converter.makeHtml(data);
+      chapterDiv.appendChild(chapterTitle);
+      chapterDiv.appendChild(chapterText);
+      document.querySelector("#selectChapter").appendChild(chapterItem);
+      return document.querySelector("#chapters").appendChild(chapterDiv);
     });
 }
 
-
-function createChapterCount(amountOfChapters){
-    let array = [];
-    for(let i=0; i < amountOfChapters; i++){
-        array.push(`chapter${i+1}`)
-    }
-    return array;
+function createChapterCount(amountOfChapters) {
+  let array = [];
+  for (let i = 0; i < amountOfChapters; i++) {
+    array.push(`chapter${i + 1}`);
+  }
+  return array;
 }
+
+// THEME SWITCHER CODE
+
+let currentTheme = "dark";
+
+$("#themeSwitcherIcon").on("click", () => {
+  if (currentTheme === "dark") {
+    $("body").css("background-color", "rgb(236, 236, 236)");
+    $("body").css("color", "black");
+    $("a").css("color", "black");
+    $("#themeSwitcher").css("filter", "invert(0)");
+    $("#themeSwitcherIcon").attr("src", "./assets/images/moon.svg");
+    currentTheme = "light";
+  } else {
+    $("body").css("background-color", "rgb(19, 19, 19)");
+    $("body").css("color", "white");
+    $("a").css("color", "white");
+    $("#themeSwitcher").css("filter", "invert(1)");
+    $("#themeSwitcherIcon").attr("src", "./assets/images/sun.svg");
+    currentTheme = "dark";
+  }
+});
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
-const { resolve } = require('path')
-const { defineConfig } = require('vite')
+const { resolve } = require("path");
+const { defineConfig } = require("vite");
 
 module.exports = defineConfig({
   build: {
+    target: "esnext",
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        novel: resolve(__dirname, 'novel.html')
-      }
-    }
-  }
-})
+        main: resolve(__dirname, "index.html"),
+        novel: resolve(__dirname, "novel.html"),
+      },
+    },
+  },
+});
